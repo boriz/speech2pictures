3ae18a36b38fbd2296bc1e5ea7192e519ded4fbc
Boris <cherkasskiy@gmail.com>
2023-06-11 22:21:45 -0600

    Add config class template.

--- config_template.py
+++ config_template.py
@@ -4,10 +4,14 @@ class config:
     model_name = "medium"   # "tiny", "base", "small", "medium", "large"
     english_language = True
     phrase_timeout_sec = 30  # Phrase timeout
-    image_refresh_sec = 300  # How often to refresh the picture
+    image_refresh_sec = 120 #300  # How often to refresh the picture
     gpt_api_key = "sk-<>"
     gpt_model = "gpt-3.5-turbo"
-    gpt_prompt = "The following text is too long, make it shorter, less than 300 characters. Also, make the text sound like a painitng title. Source text: \n"    
+    gpt_prompt = "You are a visual art AI. \
+    You'll be provided with a transcript and you'll decide if it should be a photo or a painting based on the transcript. \
+    Generate the follwoing fields: Title, Style, Description. \
+    Description should be less than 300 characters. \
+    Here is the transcript: \n"
     image_model = "stabilityai/stable-diffusion-2-1"
-    
-    
+    db_file_name = "image_database.sqlite"
+	
